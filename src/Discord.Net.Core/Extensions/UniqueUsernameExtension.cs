ee3bc745cf1122f41751ca96b5dca8d4c810e2a5
KaKusaOAO <ka.kusaowo@gmail.com>
2023-06-30 14:14:11 +0800

    Discord.Net: Fixed mistaken note in comments

--- src/Discord.Net.Core/Extensions/UniqueUsernameExtension.cs
+++ src/Discord.Net.Core/Extensions/UniqueUsernameExtension.cs
@@ -6,7 +6,7 @@ public static class UniqueUsernameExtension
 {
     // According to the new username restrictions, a valid username must:
     // - Be between 2 and 32 characters long.
-    // - Contain only lower-case alphanumeric characters, underscores, dots and dashes.
+    // - Contain only lower-case alphanumeric characters, underscores, dots.
     // - Not contain two consecutive dots.
     private static bool ValidateUsername(string name) =>
         Regex.IsMatch(name, "^(?!.*\\.\\.)[a-z0-9\\._]{2,32}$");
